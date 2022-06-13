e5febbf46ce12edfca951cd1bd15cc99c31bd1a2
dronline872 <dronline872@gmail.com>
2022-06-13 19:54:21 +0300

    Урок 5

--- internal/findfiles/findfile.go
+++ internal/findfiles/findfile.go
@@ -1,17 +1,12 @@
-package main
-
-//Исходники задания для первого занятия у других групп https://github.com/t0pep0/GB_best_go
+package findfiles
 
 import (
 	"context"
 	"fmt"
 	"io/ioutil"
 	"os"
-	"os/signal"
 	"path/filepath"
 	"sync"
-	"syscall"
-	"time"
 
 	"go.uber.org/zap"
 )
@@ -94,42 +89,4 @@ func FindFiles(ctx context.Context, ext string, sigChUsr chan os.Signal, logger 
 	}
 
 	return ret, nil
-}
-
-func main() {
-	logger, _ := zap.NewProduction()
-	const wantExt = ".go"
-	ctx := context.Background()
-	ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
-	defer cancel()
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
-
-	sigChUsr := make(chan os.Signal, 1)
-	signal.Notify(sigChUsr, syscall.SIGUSR1)
-
-	waitCh := make(chan struct{})
-	go func() {
-		res, err := FindFiles(ctx, wantExt, sigChUsr, logger)
-		if err != nil {
-			logger.Error(fmt.Sprintf("Error on search: %v\n", err))
-			os.Exit(1)
-		}
-
-		for _, f := range res {
-			logger.Info("Result:",
-				zap.String("Name", f.Name),
-				zap.String("Path", f.Path),
-			)
-		}
-		waitCh <- struct{}{}
-	}()
-	go func() {
-		<-sigCh
-		logger.Warn("Signal received, terminate...")
-		cancel()
-	}()
-	//Дополнительно: Ожидание всех горутин перед завершением
-	<-waitCh
-	logger.Info("Done")
 }
